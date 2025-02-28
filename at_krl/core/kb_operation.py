3aa46d1929c3c62375f2eaa73ad0843ade97ce07
grigadnal625 <grigandal625@gmail.com>
2025-02-28 12:19:25 +0300

    add from simple

--- at_krl/core/kb_operation.py
+++ at_krl/core/kb_operation.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from at_krl.core.kb_value import Evaluatable
+from at_krl.core.non_factor import NonFactor
 from at_krl.core.simple.simple_operation import SimpleOperation
 
 if TYPE_CHECKING:
@@ -10,4 +11,11 @@ if TYPE_CHECKING:
 
 @dataclass(kw_only=True)
 class KBOperation(Evaluatable, SimpleOperation):
-    pass
+    @staticmethod
+    def from_simple(op: SimpleOperation):
+        return KBOperation(
+            sign=op.sign,
+            left=op.left.from_simple(op.left),
+            right=op.right.from_simple(op.right) if op.right else None,
+            non_factor=NonFactor(),
+        )
